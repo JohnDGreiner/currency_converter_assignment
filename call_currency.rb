2c2675bc7bb29187ba5f9afe030ff90625b7cc4b
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-07 13:07:37 -0400

    create currency class with code method

---
+++ call_currency.rb
@@ -0,0 +1,4 @@
+require "./currency.rb"
+
+money = Currency.new(1.00,"EUR")
+puts money.code
---
+++ currency.rb
@@ -0,0 +1,12 @@
+class Currency
+
+  def initialize(amount = 0.0,currency_code = "USD")
+    @amount = amount
+    @currency_code = currency_code
+  end
+
+  def code
+    @currency_code
+  end
+
+end
