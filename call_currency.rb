f21e1abc958cc3196e22ad64aa4a4dd3c31a8513
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-07 13:09:24 -0400

    create amount method

--- call_currency.rb
+++ call_currency.rb
@@ -2,3 +2,4 @@ require "./currency.rb"
 
 money = Currency.new(1.00,"EUR")
 puts money.code
+puts money.amount
--- currency.rb
+++ currency.rb
@@ -1,12 +1,16 @@
 class Currency
 
-  def initialize(amount = 0.0,currency_code = "USD")
+  def initialize(amount = 0.0, currency_code = "USD")
     @amount = amount
     @currency_code = currency_code
   end
 
   def code
     @currency_code
+  end
+
+  def amount
+    @amount
   end
 
 end
